041af37e4c35a229f37467ab987a83264b2c9e8f
Thomas Levine <occurrence@thomaslevine.com>
2013-07-20 17:44:48 -0700

    now they get different values

--- cubic-error.R
+++ cubic-error.R
@@ -1,13 +1,14 @@
-x <- 1:10
+x <- seq(0, 10, 0.1)
+y <- rpois(100, 3)
 
-abs.error <- sapply(x, function(i){sum(abs(iris$Sepal.Length - i))})
-squared.error <- sapply(x, function(i){sum((iris$Sepal.Length - i))})
-cubed.error <- sapply(x, function(i){sum((iris$Sepal.Length - i))})
+abs.error <- sapply(x, function(i){    sum(abs( (y - i)^1) )})
+squared.error <- sapply(x, function(i){sum(abs( (y - i)^2) )})
+cubed.error <- sapply(x, function(i){  sum(abs( (y - i)^3) )})
 
 plot(cubed.error ~ x, type = 'l')
 lines(squared.error ~ x, col = 2)
 lines(abs.error ~ x, col = 3)
 
-print(x[order(abs(abs.error))[1]])
-print(x[order(abs(squared.error))[1]])
-print(x[order(abs(cubed.error))[1]])
+print(x[order(abs.error)[1]])
+print(x[order(squared.error)[1]])
+print(x[order(cubed.error)[1]])
