bdea1770d0b0d8b49fa5c3cdc9634c09a0d665eb
Thomas Levine <occurrence@thomaslevine.com>
2013-07-20 17:47:42 -0700

    aoeu

--- cubic-error.R
+++ cubic-error.R
@@ -1,14 +1,13 @@
 x <- seq(0, 10, 0.1)
 y <- rpois(100, 3)
 
-abs.error <- sapply(x, function(i){    sum(abs( (y - i)^1) )})
-squared.error <- sapply(x, function(i){sum(abs( (y - i)^2) )})
-cubed.error <- sapply(x, function(i){  sum(abs( (y - i)^3) )})
 
-plot(cubed.error ~ x, type = 'l')
-lines(squared.error ~ x, col = 2)
-lines(abs.error ~ x, col = 3)
+n.error <- function(n){
+  sapply(x, function(i) { sum(abs( (y-i)^n )) })
+}
 
-print(x[order(abs.error)[1]])
-print(x[order(squared.error)[1]])
-print(x[order(cubed.error)[1]])
+
+plot(n.error(6) ~ x, col = 6, type = 'l', ylim = c(0, 10))
+for (m in 1:5) {
+  lines(n.error(1), col = m)
+}
