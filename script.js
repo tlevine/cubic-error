8d0fc0c3f7928398314452498a6da9e31eed648f
Thomas Levine <occurrence@thomaslevine.com>
2013-08-02 15:19:33 -0700

    aoeu

--- script.js
+++ script.js
@@ -17,6 +17,13 @@
     return _sample
   }
 
+  viz.ticks = function(n) {
+    var vec = []
+    for (var i = 0; i < n; i++) {
+      vec.push(i / n)
+    }
+  }
+
   viz.error = function(n) {
     // Polynomial-n error metric
     return function(a, b) {
@@ -24,18 +31,28 @@
     }
   }
 
-  var SIDE = 840
+  var SIDE = 640
   var identity = function(d) { return d}
 
   viz.viz = d3.select("#viz")
-            .append('svg').attr('width', SIDE).attr('height', SIDE)
-            .selectAll('rect')
-            .data(viz.sample(viz.skewedDistribution, 10))
-            .enter()
-            .append('rect')
-            .attr('x', function(d) { return SIDE * d })
-            .attr('y', 0)
-            .attr('height', 10)
-            .attr('width', 10)
-// <rect x="0" y="0" width="500" height="50"/>
+    .append('svg').attr('width', SIDE).attr('height', SIDE)
+
+  /*
+  viz.viz.selectAll('rect')
+    .data(viz.sample(viz.skewedDistribution, 10))
+    .enter()
+    .append('rect')
+    .attr('x', function(d) { return SIDE * d })
+    .attr('y', 0)
+    .attr('height', 10)
+    .attr('width', 10)
+  */
+
+  viz.viz.append('line')
+    .attr('x1', 0).attr('y1', SIDE)
+    .attr('x2', SIDE).attr('y1', SIDE)
+    .attr('stroke-width', 4)
+    .selectAll('line')
+    .data(viz.ticks(100))
+    .enter()
 })()
